4c2cf96b179f0bd1942dbe1c3154aa71b4282b1e
SonAuJiLi <zzilssun@gmail.com>
2022-11-16 20:45:23 +0900

    feat: Complete ThemingCodelab

---
+++ pathway1-2/ThemingCodelab/app/src/main/java/com/codelab/theming/theme/Shape.kt
@@ -0,0 +1,15 @@
+package com.codelab.theming.theme
+
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+/**
+ * Created by Son Aujili on 2022/11/16.
+ */
+val JetnewsShapes = Shapes(
+    small = CutCornerShape(topStart = 8.dp),
+    medium = CutCornerShape(topStart = 24.dp),
+    large = RoundedCornerShape(8.dp)
+)
